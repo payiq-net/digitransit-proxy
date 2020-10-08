45c87ca2033f86c4544c12faf1555bd54ce2ec3f
Ristomatti Karppanen <ristomatti.karppanen@cgi.com>
2020-10-08 15:57:40 +0300

    travis-build.sh changed to look like the template from other repos

--- travis-build.sh
+++ travis-build.sh
@@ -23,6 +23,13 @@ DOCKER_IMAGE_TAG=$DOCKER_IMAGE:$DOCKER_TAG
 DOCKER_IMAGE_TAG_LONG=$DOCKER_IMAGE:$DOCKER_TAG_LONG
 
 
+if [ -z $TRAVIS_TAG ]; then
+  # Build image
+  echo "Building digitransit-proxy"
+  ./test.sh
+  docker build --tag=$DOCKER_IMAGE_TAG_LONG -f Dockerfile .
+fi
+
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   docker login -u ${DOCKER_USER} -p ${DOCKER_AUTH}
   if [ "$TRAVIS_TAG" ];then
@@ -41,10 +48,6 @@ if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     docker tag $DOCKER_IMAGE_TAG_LONG $DOCKER_IMAGE_TAG
     docker push $DOCKER_IMAGE_TAG
   fi
-else
-  echo "processing pr $TRAVIS_PULL_REQUEST"
-  ./test.sh
-  docker build  --tag=$DOCKER_IMAGE_TAG_LONG -f Dockerfile .
 fi
 
 echo Build completed
