be016d424896172d7e73ce888311d5d3c8a74b1b
Joel Lappalainen <lappalj8@gmail.com>
2021-06-08 09:52:21 +0300

    Add salo realtime external endpoint

--- run.sh
+++ run.sh
@@ -29,6 +29,7 @@ sed -i "s/dev.hslfi.hsldev.com/${NEW_HSL_FI_URL}/" /etc/nginx/nginx.conf
 sed -i "s/LMJ_BASIC_AUTH/${LMJ_BASIC_AUTH}/" /etc/nginx/external.conf
 sed -i "s/MIKKELI_BASIC_AUTH/${MIKKELI_BASIC_AUTH}/" /etc/nginx/external.conf
 sed -i "s/VAASA_BASIC_AUTH/${VAASA_BASIC_AUTH}/" /etc/nginx/external.conf
+sed -i "s/SALO_BASIC_AUTH/${SALO_BASIC_AUTH}/" /etc/nginx/external.conf
 
 #start nginx
 nginx
