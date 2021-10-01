99ed076739aa7fe43ab30fcf8016fc67dd1c5801
Joel Lappalainen <lappalj8@gmail.com>
2021-10-01 09:21:25 +0300

    Temporarily disable map api

--- test.js
+++ test.js
@@ -118,8 +118,8 @@ describe('api.digitransit.fi', function() {
  // testCaching('api.digitransit.fi','/realtime/vehicle-positions/v1/foo',false);
   testProxying('api.digitransit.fi','/realtime/raildigitraffic2gtfsrt/v1/','raildigitraffic2gtfsrt:8080');
  //testCaching('api.digitransit.fi','/realtime/raildigitraffic2gtfsrt/v1/foo',true);
-  testProxying('api.digitransit.fi','/map/v1/','hsl-map-server:8080');
-  testProxying('api.digitransit.fi','/map/v1/next-hsl-map/index.json','hsl-map-server-next:8080');
+  // testProxying('api.digitransit.fi','/map/v1/','hsl-map-server:8080');
+  // testProxying('api.digitransit.fi','/map/v1/next-hsl-map/index.json','hsl-map-server-next:8080');
   testProxying('api.digitransit.fi','/routing/v1/routers/finland','opentripplanner-finland:8080');
   testProxying('api.digitransit.fi','/routing/v1/routers/hsl','opentripplanner-hsl:8080');
   testProxying('api.digitransit.fi','/routing/v1/routers/waltti','opentripplanner-waltti:8080');
