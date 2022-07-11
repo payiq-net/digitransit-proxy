9273a363afb8902b0c0aff8f1e82ce5c1f7d7534
Joona Ojapalo <joona.ojapalo@cgi.fi>
2022-07-11 11:32:46 +0300

    Added basic auth route to Mattersoft/VARELY

--- run.sh
+++ run.sh
@@ -23,6 +23,7 @@ sed -i "s/PERILLE_BASIC_AUTH/${PERILLE_BASIC_AUTH}/" /etc/nginx/external.conf
 sed -i "s/GIRAVOLTA_TAMPERE_AUTH/${GIRAVOLTA_TAMPERE_AUTH}/" /etc/nginx/external.conf
 sed -i "s/KOTKA_BASIC_AUTH/${KOTKA_BASIC_AUTH}/" /etc/nginx/external.conf
 sed -i "s/GIRAVOLTA_VANTAA_AUTH/${GIRAVOLTA_VANTAA_AUTH}/" /etc/nginx/external.conf
+sed -i "s/VARELY_BASIC_AUTH/${VARELY_BASIC_AUTH}/" /etc/nginx/external.conf
 
 #start nginx
 nginx
